d411abdcd62f8727be6e145fe65909e38ec907c3
wafflezz <56751225+waffflezz@users.noreply.github.com>
2023-09-11 22:09:04 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Main class
+ */
 public class Main  {
 
+  /**
+   * Entry point of program
+   *
+   * @param args arguments you can pass into program from console
+   */
   public static void main(String[] args) {
     Menu menu = new Menu();
     menu.start();
